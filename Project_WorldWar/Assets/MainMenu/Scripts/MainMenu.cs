a41db8c2c6527e96197504c0eff1b36595010bd5
Bourne_art <art.rahmatullin@gmail.com>
2018-11-11 23:06:13 +0300

    Main Menu SelectionMenu Ptototype v.0.0.1a

---
+++ Project_WorldWar/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    #region Нажатие кнопки Start Game
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    #endregion
+
+    #region Нажатие кнопки Settings
+    public void SettingsMenu()
+    {
+
+    }
+    #endregion
+
+    #region Нажатие кнопки Exit
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
